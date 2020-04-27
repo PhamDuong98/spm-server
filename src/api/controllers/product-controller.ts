d2e86b89d8f9d9723a4bed9da57d8fe55c1fbce6
dotrungduc <duc010298@gmail.com>
2020-04-27 22:14:10 +0700

    fix tslint

--- src/api/controllers/product-controller.ts
+++ src/api/controllers/product-controller.ts
@@ -10,7 +10,16 @@ class ProductController {
     public static getAll = async (req: Request, res: Response) => {
         try {
             const productRepository = getRepository(Product);
-            const allProducts = await productRepository.find();
+            const allProducts = await productRepository.createQueryBuilder("product")
+                .leftJoinAndSelect("product.unit", "unit")
+                .leftJoinAndSelect("product.category", "category")
+                .getMany();
+            for (const prod of allProducts) {
+                prod["unitName"] = prod.unit.name;
+                delete prod.unit;
+                prod["categoryName"] = prod.category.name;
+                delete prod.category;
+            }
 
             const user: any = res.locals.user;
 
